d9f6bdef26281ff911c51e80cc23caee5f8afa5a
iArty303 <106126876+iArty303@users.noreply.github.com>
2025-08-16 23:40:36 -0600

    Update fulfillmentprocess.js

--- fulfillmentprocess.js
+++ fulfillmentprocess.js
@@ -1,4 +1,13 @@
-
+//Update on Info Plus for Orders with Future Ship Dates Solution:
+ 
+//Here is how the script works: 
+//It is set to run every day during business hours and runs through every order we have in the On Order queue with the 1-Hour Processing Delay.
+//If there is a First Ship Date in the Order for a future ship date, the script will automatically assign it a Hold Code of H to hold the order till a future date that matches the current day.
+//If the First Ship Date is empty, the script will not get run.
+//If the First Ship Date is equal to the current day, the script will automatically change the hold to Y and release the order from on hold.
+//The fulfillment plan will still run as usual, processing orders that are 1 hour old and do not have any hold codes or hold tags in them.
+//With all this, this will help ease the management of overseeing these orders.
+ 
 
 // Getting Current Date and formatting to only year, month, and day.
 var today = new Date();
